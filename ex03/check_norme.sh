2e8b492af72dae6ca804db21d09296164b962336
vvaucoul <vvaucoul@student.42.fr>
2022-06-08 18:21:43 +0200

    Update

---
+++ ex03/check_norme.sh
@@ -0,0 +1,1 @@
+/usr/src/linux-headers-5.4.0-113-generic/scripts/checkpatch.pl main.c -no-tree -file --strict
--- ex03/main.c
+++ ex03/main.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: <GPL-2.0+>
 #include <linux/module.h>
 #include <linux/kernel.h>
 #include <linux/delay.h>
@@ -5,30 +6,30 @@
 
 int do_work(int *my_int, int retval)
 {
-        int x;
-        int y = *my_int;
-        int z;
+		int x;
+		int y = *my_int;
+		int z;
 
-        for (x = 0; x < my_int; ++x)
-               udelay(10);
-        if (y < 10)
-        {
-               /* 
-               ** That was a long sleep, tell userspace about it
-               */
-               pr_info("We slept a long time!");
-        }
-        z = x * y;
-        return z;
+	for (x = 0; x < my_int; ++x)
+		usleep(10);
+	if (y < 10) {
+		/*
+		 ** That was a long sleep, tell userspace about it
+		 */
+		pr_info("We slept a long time!");
+	}
+	z = x * y;
+	return z;
 }
 
 int my_init(void)
 {
-        int x = 10;
+		int x = 10;
 
-        x = do_work(&x, x);
-        return x;
+		x = do_work(&x, x);
+		return x;
 }
+
 void my_exit(void)
 {
 }
